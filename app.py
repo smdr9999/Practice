7cd67f107395a1ec9c99b2b4144c5ef3d9dbd34d
Bogala Sai Mani Devendar Reddy <168011488+smdr9999@users.noreply.github.com>
2025-05-21 12:19:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import smtplib
+from flask import Flask, request, jsonify
+from email.mime.text import MIMEText
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Gmail Email API is active."
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    data = request.get_json()
+    text = data.get("text", "")
+
+    msg = MIMEText(text)
+    msg["Subject"] = "Typed Data"
+    msg["From"] = os.environ["EMAIL_FROM"]
+    msg["To"] = os.environ["EMAIL_TO"]
+
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(os.environ["EMAIL_FROM"], os.environ["EMAIL_PASSWORD"])
+            server.send_message(msg)
+        return jsonify({"status": "Email sent"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
